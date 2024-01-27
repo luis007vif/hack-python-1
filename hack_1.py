04f73d629d8b0e9588fa412786a5f283bf945328
luis007vif <luisraul29082002@gmail.com>
2024-01-26 21:34:17 -0400

    Hack completo de python

---
+++ hack_1.py
@@ -0,0 +1,13 @@
+"""
+text: "fooziman" output => "FOOZIMAN"
+"""
+
+def fn_hack_1():
+    result = "fooziman"
+
+    return result.upper()  
+
+r = fn_hack_1()
+    
+print(r)
+    
---
+++ hack_5.py
@@ -0,0 +1,11 @@
+"""
+text: "fooziman" output => "f00z1m@n"
+"""
+
+def fn_hack_5():
+    result = "fooziman"
+    result = result.replace('o', '0').replace('i', '1').replace('a','@')
+    return result
+
+r = fn_hack_5()
+print(r)
